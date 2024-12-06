edc9deda167b373eac4463fc4c7b2846c5bed5e3
ThierryMa <tmarchandise@axway.com>
2024-12-06 17:10:56 +0100

    Delete assets/script.js

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('pre').addEventListener('click', function() {
-  const codeContent = this.innerText;
-  navigator.clipboard.writeText(codeContent).then(function() {
-    alert('Code copied to clipboard!');
-  }, function(err) {
-    console.error('Could not copy text: ', err);
-  });
-});
