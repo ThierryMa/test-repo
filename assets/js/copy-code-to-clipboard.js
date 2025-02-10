7fb227672728c22450bc24a088a7a58466e7d0e1
ThierryMa <tmarchandise@axway.com>
2025-02-10 12:05:52 +0100

    Update copy-code-to-clipboard.js

--- assets/js/copy-code-to-clipboard.js
+++ assets/js/copy-code-to-clipboard.js
@@ -1,8 +1,10 @@
-document.querySelector('pre').addEventListener('click', function() {
-  const codeContent = this.innerText;
-  navigator.clipboard.writeText(codeContent).then(function() {
-    console.info('Code copied to clipboard');
-  }, function(err) {
-    console.error('Could not copy text: ', err);
+document.querySelectorAll('pre').forEach(function(pre) {
+  pre.addEventListener('click', function() {
+    const codeContent = this.innerText;
+    navigator.clipboard.writeText(codeContent).then(function() {
+      console.info('Code copied to clipboard');
+    }, function(err) {
+      console.error('Could not copy text: ', err);
+    });
   });
 });
